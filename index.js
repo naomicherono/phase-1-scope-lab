1ee1582158a463bac826d328ac2ea271c5fa61e7
naomicherono <nonniecheronoh12@gmail.com>
2023-06-16 14:01:50 +0300

    Completd assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+let customerName;
+
+function declareCustomerName() {
+  window.customerName = 'bob';
+}
+
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+
+declareCustomerName();
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new person';
+}
